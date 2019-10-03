041064dfb4e44c956a4652da3129bbc8e44afac5
kg-coderta <ke123iji@gmail.com>
2019-10-03 12:39:33 +0900

    set routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :users, only: [:edit, :update,:destroy]
+  resources :users, only: [:edit, :update,:index]
+  resources :groups, only: [:new, :create, :update,:edit]  do
+    resources :messages, only: [:index, :create]
+  end
 end
