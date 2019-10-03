b7b69300e75d2f77683d4ceeb368772391c14cdb
kg-coderta <ke123iji@gmail.com>
2019-10-03 14:34:31 +0900

    change root path and file directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "groups#index"
   resources :users, only: [:edit, :update,:index]
   resources :groups, only: [:new, :create, :update,:edit]  do
     resources :messages, only: [:index, :create]
